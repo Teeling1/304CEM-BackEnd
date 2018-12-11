ea89756e418057468281854aed0c108a602ea0b7
Teeling1 <teelingz@uni.coventry.ac.uk>
2018-12-11 16:46:19 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config file containing the SLC for encryption and the database connection details
+module.exports = {
+
+	'SLC': 'ab0f13',
+	'db': 'mongodb://adminXl:e3x2a16M@ds123946.mlab.com:23946/3-04-agb17'
+
+}
